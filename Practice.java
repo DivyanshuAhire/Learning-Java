e57a04806cf2789c5fe42da17752a42560424444
Divyanshu Ahire <divyanshuahire7@gmail.com>
2025-07-12 20:11:41 +0530

    3rd commit

--- Practice.java
+++ Practice.java
@@ -3,8 +3,9 @@ public class Practice
     public static void main(String[] args) 
     {
         int a = 10;
+        int b = 20;
         a = a++ + ++a;
-        System.out.println(a);
+        System.out.println(a*b);
 
 
     }
