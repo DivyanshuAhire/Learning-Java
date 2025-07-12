cdc1630ef0dbd3153385354f582d4124810c9318
Divyanshu Ahire <divyanshuahire7@gmail.com>
2025-07-13 02:19:38 +0530

    If Else Commit

--- Practice.java
+++ Practice.java
@@ -1,14 +1,25 @@
-public class Practice
-{
-    public static void main(String[] args) 
-    {
-        int a = 10;
-        int b = 20;
-        a = a++ + ++a;
-        System.out.println(a*b);
+import java.util.*;
 
-        //Hello World
+public class Practice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int age = Integer.parseInt(sc.nextLine());
 
+        if(age<13)
+        {
+            System.out.println("Child");
+        }
+        else if(age>=13 && age<=19)
+        {
+            System.out.println("teen");
+        }
+        else if(age>=20 && age<=59)
+        {
+            System.out.println("Adult");
+        }
+        else{
+            System.out.println("Senior");
+        }
     }
 }
