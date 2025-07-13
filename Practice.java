161d7ab83eda58990ade5f33b8d526440976f513
Divyanshu Ahire <divyanshuahire7@gmail.com>
2025-07-13 14:38:12 +0530

    Admission Software(Basic)

--- Practice.java
+++ Practice.java
@@ -4,22 +4,37 @@ public class Practice {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int age = Integer.parseInt(sc.nextLine());
 
-        if(age<13)
+        System.out.println("Marks?");
+        int marks = Integer.parseInt(sc.nextLine());
+
+        System.out.println("Entrance Exam Marks?");
+        int Emarks = Integer.parseInt(sc.nextLine());
+
+
+        System.out.println("Extra Curicular Activity ?? (Yes/No)");
+        String EC = sc.nextLine();
+
+
+        if (marks>=75)
         {
-            System.out.println("Child");
-        }
-        else if(age>=13 && age<=19)
-        {
-            System.out.println("teen");
-        }
-        else if(age>=20 && age<=59)
-        {
-            System.out.println("Adult");
+            if (Emarks>=120)
+            {
+                if(EC.equalsIgnoreCase("yes"))
+                {
+                    System.out.println("Eligible For Scholarship");
+                }
+                else{
+                    System.out.println("Eligible for Admission Only");
+                }
+            }
+
+            else{
+                System.out.println("Not eligible DUe to Low Entrance Score");
+            }
         }
         else{
-            System.out.println("Senior");
+            System.out.println("Not eligible DUe to Low Marks");
         }
     }
 }
