6a14c07f86166a90254e15a584bac9a1ea3bad2d
Divyanshu Ahire <divyanshuahire7@gmail.com>
2025-07-13 23:51:32 +0530

    diamond pattern

--- Practice.java
+++ Practice.java
@@ -4,22 +4,44 @@ public class Practice {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-
-        System.out.println("Enter Any Number");
+        System.out.println("Think of any Number");
         int n = Integer.parseInt(sc.nextLine());
-        
-        System.out.println("Even Numbers---");
-        for(int i=2; i<=n; i=i+2)
+       
+        for(int x=1; x<=n; x++)
+        {
+            for(int l=n-x; l>=1; l--)
             {
-                System.out.print(i + " ");
-            }
-        
-        System.out.println();
-        System.out.println("Odd Numbers");
-       for(int i=1; i<=n; i=i+2 )
-            {
-                System.out.print(i + " ");
+                System.out.print(" ");
             }
 
+            for(int y=1; y<=x; y++)
+            {
+                System.out.print("* ");
+            }
+            
+            System.out.println();
+        }
+
+
+         for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print(" ");
+            }
+           
+
+
+            for(int k=n-i; k>=1; k--)
+            {
+                System.out.print("* ");
+            }
+
+        
+            System.out.println();
+        }
+
+       
+        
     }
 }
