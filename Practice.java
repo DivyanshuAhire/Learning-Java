a64fac762e84adcf0c7ebdf55a07b22147a65065
Divyanshu Ahire <divyanshuahire7@gmail.com>
2025-07-13 18:32:50 +0530

    Loops Commit

--- Practice.java
+++ Practice.java
@@ -5,36 +5,21 @@ public class Practice {
         Scanner sc = new Scanner(System.in);
 
 
-        System.out.println("Marks?");
-        int marks = Integer.parseInt(sc.nextLine());
-
-        System.out.println("Entrance Exam Marks?");
-        int Emarks = Integer.parseInt(sc.nextLine());
-
-
-        System.out.println("Extra Curicular Activity ?? (Yes/No)");
-        String EC = sc.nextLine();
-
-
-        if (marks>=75)
-        {
-            if (Emarks>=120)
+        System.out.println("Enter Any Number");
+        int n = Integer.parseInt(sc.nextLine());
+        
+        System.out.println("Even Numbers---");
+        for(int i=2; i<=n; i=i+2)
             {
-                if(EC.equalsIgnoreCase("yes"))
-                {
-                    System.out.println("Eligible For Scholarship");
-                }
-                else{
-                    System.out.println("Eligible for Admission Only");
-                }
+                System.out.print(i + " ");
+            }
+        
+        System.out.println();
+        System.out.println("Odd Numbers");
+       for(int i=1; i<=n; i=i+2 )
+            {
+                System.out.print(i + " ");
             }
 
-            else{
-                System.out.println("Not eligible DUe to Low Entrance Score");
-            }
-        }
-        else{
-            System.out.println("Not eligible DUe to Low Marks");
-        }
     }
 }
