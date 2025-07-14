f3a8ca54f71b51dc83fda29798f148611b867d8c
Divyanshu Ahire <divyanshuahire7@gmail.com>
2025-07-14 19:22:37 +0530

    Composition of Function

--- Practice.java
+++ Practice.java
@@ -7,63 +7,41 @@ public class Practice
 
         System.out.println("Think of any Number");
         int n = Integer.parseInt(sc.nextLine());
-        //int x= Integer.parseInt(sc.nextLine());
-        System.out.println(calculatingFactorial(n));
         
-      // Diamond(n);
 
+        System.out.println("Is Even : " + isEven(n));
+        System.out.println("Is Prime : " + isPrime(n));
+        System.out.println("factorial : " + factorial(n));
     }
 
-    
-    public static int calculatingFactorial(int n)
+    public static boolean isEven(int n)
+    {
+        return n%2==0;
+    }
+
+    public static boolean  isPrime(int n)
+    {
+        if(n<1)
+        {
+            return false;
+
+        }
+        for(int j=2; j<=Math.sqrt(n); j++)
+        {
+            if(n%j==0) return false;
+        }
+        return true;
+    }
+
+    public static int factorial(int n)
     {
         int box=1;
 
-        for(int g=n; g>=1; g=g-1)
+        for(int h=n; h>=1; h--)
         {
-            box=box*g;
+            box = box * h;
         }
-
         return box;
-    }
-
-
-    public static void Diamond(int n)
-    {
-        
-        for(int x=1; x<=n; x++)
-        {
-            for(int l=n-x; l>=1; l--)
-            {
-                System.out.print(" ");
-            }
-
-            for(int y=1; y<=x; y++)
-            {
-                System.out.print("* ");
-            }
-            
-            System.out.println();
-        }
-
-
-         for(int i=1; i<=n; i++)
-        {
-            for(int j=1; j<=i; j++)
-            {
-                System.out.print(" ");
-            }
-           
-
-
-            for(int k=n-i; k>=1; k--)
-            {
-                System.out.print("* ");
-            }
-
-        
-            System.out.println();
-        }
     }
 }
 
