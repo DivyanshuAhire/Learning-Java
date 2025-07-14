71c583921e7a9f8c9856762666509cd72c95aef6
Divyanshu Ahire <divyanshuahire7@gmail.com>
2025-07-14 13:46:16 +0530

    Functions Coomit

--- Practice.java
+++ Practice.java
@@ -1,12 +1,36 @@
 import java.util.*;
 
-public class Practice {
-    public static void main(String[] args) {
+public class Practice 
+{   public static void main(String[] args) 
+    {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Think of any Number");
         int n = Integer.parseInt(sc.nextLine());
-       
+        //int x= Integer.parseInt(sc.nextLine());
+        System.out.println(calculatingFactorial(n));
+        
+      // Diamond(n);
+
+    }
+
+    
+    public static int calculatingFactorial(int n)
+    {
+        int box=1;
+
+        for(int g=n; g>=1; g=g-1)
+        {
+            box=box*g;
+        }
+
+        return box;
+    }
+
+
+    public static void Diamond(int n)
+    {
+        
         for(int x=1; x<=n; x++)
         {
             for(int l=n-x; l>=1; l--)
@@ -40,8 +64,7 @@ public class Practice {
         
             System.out.println();
         }
-
-       
-        
     }
 }
+
+
